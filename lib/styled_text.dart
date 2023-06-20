f462f9ef81bfc80f4029b1e47f57ab6254d6c0d3
pragatikalwar <129779922+pragatikalwar@users.noreply.github.com>
2023-06-20 20:02:05 +0530

    Update styled_text.dart

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 
 class Styledtext extends StatelessWidget {
    const Styledtext(this.text ,{super.key}); 
-  //  this finds the next like text in the below code and understands its data type given below and equals them if u mention string text here it doesnt understand as u have to equate it to something below
-   final  String text;
+  
   @override
   Widget build(context) {
     return  Center(
